45cf779764ea9b46ff6553592ea3496a9b70374b
Gaston Roxas <gacaro51597@gmail.com>
2023-01-29 12:19:45 +0800

    change dto name for create booking

--- src/bookings/bookings.controller.ts
+++ src/bookings/bookings.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { BookingsService } from './bookings.service';
-import { CreateBookingDto } from './dto/bookings.dto';
+import { CreateBookingDto } from './dto/create-booking.dto';
 
 @Controller('bookings')
 export class BookingsController {
