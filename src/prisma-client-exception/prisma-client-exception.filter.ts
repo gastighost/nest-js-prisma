26151516250eb0ad2922aa239803228b074490e2
Gaston Roxas <gacaro51597@gmail.com>
2023-01-29 11:17:21 +0800

    add extra error handling code to prisma error exception filter

--- src/prisma-client-exception/prisma-client-exception.filter.ts
+++ src/prisma-client-exception/prisma-client-exception.filter.ts
@@ -3,34 +3,62 @@ import { BaseExceptionFilter } from '@nestjs/core';
 import { Prisma } from '@prisma/client';
 import { Response } from 'express';
 
-@Catch(Prisma.PrismaClientKnownRequestError)
+@Catch(
+  Prisma.PrismaClientKnownRequestError,
+  Prisma.PrismaClientUnknownRequestError,
+  Prisma.PrismaClientValidationError,
+)
 export class PrismaClientExceptionFilter extends BaseExceptionFilter {
-  catch(exception: Prisma.PrismaClientKnownRequestError, host: ArgumentsHost) {
+  catch(
+    exception:
+      | Prisma.PrismaClientKnownRequestError
+      | Prisma.PrismaClientUnknownRequestError
+      | Prisma.PrismaClientValidationError,
+    host: ArgumentsHost,
+  ) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const message = exception.message.replace(/\n/g, '');
 
-    switch (exception.code) {
-      case 'P2002': {
-        const status = HttpStatus.CONFLICT;
-        response.status(status).json({
-          statusCode: status,
-          message: message,
-        });
-        break;
+    if (exception instanceof Prisma.PrismaClientKnownRequestError) {
+      switch (exception.code) {
+        case 'P2002': {
+          const status = HttpStatus.CONFLICT;
+          response.status(status).json({
+            statusCode: status,
+            message: message,
+          });
+          break;
+        }
+        case 'P2025': {
+          const status = HttpStatus.NOT_FOUND;
+          response.status(status).json({
+            statusCode: status,
+            message: message,
+          });
+          break;
+        }
+        default:
+          // default 500 error code
+          super.catch(exception, host);
+          break;
       }
-      case 'P2025': {
-        const status = HttpStatus.NOT_FOUND;
-        response.status(status).json({
-          statusCode: status,
-          message: message,
-        });
-        break;
-      }
-      default:
-        // default 500 error code
-        super.catch(exception, host);
-        break;
+    }
+
+    if (exception instanceof Prisma.PrismaClientUnknownRequestError) {
+      const status = HttpStatus.INTERNAL_SERVER_ERROR;
+      response.status(status).json({
+        statusCode: status,
+        message: message,
+      });
+    }
+
+    if (exception instanceof Prisma.PrismaClientValidationError) {
+      const status = HttpStatus.BAD_REQUEST;
+      response.status(status).json({
+        statusCode: status,
+        message: message,
+      });
     }
   }
 }
