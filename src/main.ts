6ffdfe783491013f87f7a444b3462cd4fae18a7d
Gaston Roxas <gacaro51597@gmail.com>
2023-01-29 10:53:52 +0800

    add prisma error exception handler

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaClientExceptionFilter } from './prisma-client-exception/prisma-client-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.setGlobalPrefix('api');
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
+
   await app.listen(8080);
 }
 bootstrap();
