0d5eddb04e1f1e6d6eb8906229224eb7c9c752d6
Gaston Roxas <gacaro51597@gmail.com>
2023-02-02 22:14:59 +0800

    change features folder name to model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
-import { BookingsModule } from './features/bookings/bookings.module';
+import { BookingsModule } from './models/bookings/bookings.module';
 import { PrismaModule } from './services/prisma/prisma.module';
-import { UsersModule } from './features/users/users.module';
+import { UsersModule } from './models/users/users.module';
 import { AuthModule } from './services/auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { WebsocketsModule } from './services/websockets/websockets.module';
