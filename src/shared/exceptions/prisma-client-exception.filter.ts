a30697826a3b58f824b8294290b07f5049711b16
Gaston Roxas <gacaro51597@gmail.com>
2023-02-03 15:04:25 +0800

    enhance prisma client error handler

--- src/shared/exceptions/prisma-client-exception.filter.ts
+++ src/shared/exceptions/prisma-client-exception.filter.ts
@@ -26,7 +26,7 @@ export class PrismaClientExceptionFilter extends BaseExceptionFilter {
           const status = HttpStatus.CONFLICT;
           response.status(status).json({
             statusCode: status,
-            message: message,
+            message: exception.meta.cause || message,
           });
           break;
         }
@@ -34,7 +34,7 @@ export class PrismaClientExceptionFilter extends BaseExceptionFilter {
           const status = HttpStatus.NOT_FOUND;
           response.status(status).json({
             statusCode: status,
-            message: message,
+            message: exception.meta.cause || message,
           });
           break;
         }
